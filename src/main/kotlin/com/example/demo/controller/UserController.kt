88d09fc6df2914b5ca1f4b5351084bebb66ecf18
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-20 15:29:55 +0300

    Add create, get and update for user

---
+++ src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -0,0 +1,49 @@
+package com.example.demo.controller
+
+import com.example.demo.entity.User
+import com.example.demo.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/users")
+class UserController(@Autowired private val userRepository: UserRepository) {
+
+    @GetMapping
+    fun getAllUsers(): List<User> = userRepository.findAll().toList();
+
+    @PostMapping
+    fun createUser(@RequestBody user: User): ResponseEntity<User> {
+        val createdUser = userRepository.save(user)
+        return ResponseEntity(createdUser, HttpStatus.CREATED)
+    }
+
+    @GetMapping
+    fun getUserById(@PathVariable("id") userId: Long): ResponseEntity<User> {
+        val user = userRepository.findById(userId).orElse(null)
+        return if (user != null) ResponseEntity(user, HttpStatus.OK)
+        else ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    @GetMapping("/{id}")
+    fun updateUser(@RequestBody user:User, @PathVariable("id") userId:Long): ResponseEntity<User> {
+        val existingUser = userRepository.findById(userId).orElse(null)
+        if(existingUser == null) {
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+
+        val updatedUser = existingUser.copy(name = user.name, email = user.email)
+        userRepository.save(updatedUser)
+        return ResponseEntity(updatedUser, HttpStatus.OK)
+    }
+
+
+}
