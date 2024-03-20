a16df9dd5d6cdf86a3273c6d71cb3098b4f5f4bc
Titus Muthomi <muthomititus+github@gmail.com>
2024-03-20 15:44:31 +0300

    Add Kotlin CRUD

--- src/main/kotlin/com/example/demo/controller/UserController.kt
+++ src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -4,12 +4,12 @@ import com.example.demo.entity.User
 import com.example.demo.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -27,14 +27,14 @@ class UserController(@Autowired private val userRepository: UserRepository) {
         return ResponseEntity(createdUser, HttpStatus.CREATED)
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     fun getUserById(@PathVariable("id") userId: Long): ResponseEntity<User> {
         val user = userRepository.findById(userId).orElse(null)
         return if (user != null) ResponseEntity(user, HttpStatus.OK)
         else ResponseEntity(HttpStatus.NOT_FOUND)
     }
 
-    @GetMapping("/{id}")
+    @PutMapping("/{id}")
     fun updateUser(@RequestBody user: User, @PathVariable("id") userId: Long): ResponseEntity<User> {
         val existingUser = userRepository.findById(userId).orElse(null)
         if (existingUser == null) {
